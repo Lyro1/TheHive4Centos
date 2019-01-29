fac71673e52ac569406e55fe2cd7e9172af53ef9
Louis Dupont <contact@lyro.fr>
2019-01-29 09:34:06 +0100

    Removed ElasticSearch start-up
    ElasticSearch should not be started since it is not configurated yet.

--- thehive_installer_centos.sh
+++ thehive_installer_centos.sh
@@ -10,12 +10,3 @@ sudo wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.5
 sudo rpm -i elasticsearch-6.5.4.rpm
 
 echo "[ OK ] ElasticSearch installed"
-
-echo "[INFO] Starting ElasticSearch..."
-sudo -i service elasticsearch start
-sudo journalctl -f
-
-echo "[INFO] Checking if ElasticSearch is running..."
-curl -X GET "localhost:9200/"
-
-echo "[ OK ] Testing done. Open http://localhost:9000"
